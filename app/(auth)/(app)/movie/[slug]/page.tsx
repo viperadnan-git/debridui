500aba24db2a209f40a3c3238974a645c9bb6e1f
Adnan Ahmad <viperadnan@gmail.com>
2026-01-30 13:40:33 +0000

    refactor: remove dyanmic imports of media details for ux improvement

--- app/(auth)/(app)/movie/[slug]/page.tsx
+++ app/(auth)/(app)/movie/[slug]/page.tsx
@@ -1,22 +1,10 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import { useTraktMedia } from "@/hooks/use-trakt";
 import { useParams } from "next/navigation";
 import { memo } from "react";
 import { MdbFooter } from "@/components/mdb/mdb-footer";
-
-const MediaDetails = dynamic(
-    () => import("@/components/mdb/media-details").then((m) => ({ default: m.MediaDetails })),
-    {
-        loading: () => (
-            <div className="w-full space-y-4">
-                <div className="h-[35vh] bg-muted/30 animate-pulse" />
-                <div className="h-24 bg-muted/30 rounded-sm animate-pulse" />
-            </div>
-        ),
-    }
-);
+import { MediaDetails } from "@/components/mdb/media-details";
 
 const MoviePage = memo(function MoviePage() {
     const params = useParams();
